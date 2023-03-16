864f8a54d1ac0547c6d8b21e59a710dc3ade24ce
massi00 <manpreetsinghassi@gmail.com>
2023-03-15 21:41:17 -0700

    Hamburger v3

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-console.log("yes!");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#menu");
 
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
-const menu_btn1 = document.querySelector('.hamburger1');
+hamburger.addEventListener("click", mobileMenu);
 
-menu_btn.addEventListener('click', function() {
-    // console.log("yes!");
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-});
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    
+    // Add the following lines to show/hide the menu
+    if (navMenu.classList.contains("active")) {
+      navMenu.style.opacity = "1";
+      navMenu.style.visibility = "visible";
+    } else {
+      navMenu.style.opacity = "0";
+      navMenu.style.visibility = "hidden";
+    }
+  }
 
-menu_btn1.addEventListener('click', function() {
-    console.log("yes!");
-    menu_btn1.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-});
